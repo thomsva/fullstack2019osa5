e6536150af9dab60b561d47a114156bd2159c380
Thomas Svartbäck <thomsva@gmail.com>
2019-02-16 14:50:07 +0200

    5.2 valmis

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -13,10 +13,21 @@ const App = () => {
 
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
   }, [])
+
+
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -24,6 +35,10 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
 
       setUser(user)
       setUsername('')
@@ -34,6 +49,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
   }
 
 
@@ -69,7 +89,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <div>{user.name} logged in</div>
+      <button onClick={handleLogout}>log out</button>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
